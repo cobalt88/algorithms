536a8e895d6281005aed72dc36f63ae9f1db760f
cobalt88 <vincentteune@gmail.com>
2022-08-12 11:49:44 -0400

    8.12.22
    Updated wierdNums.js description and constraints

--- wierdNums.js
+++ wierdNums.js
@@ -11,6 +11,8 @@ The goal of this algorithm is to produce all numbers with this property within a
 
 for example if n = 1, return the zeroth position of the array (81)
 if n = 30 return the 29th position of the array(248155780267521)
+Note: this algorithm in javascript can only handle up to the 31st number with this property 3904305912313344, since after this the next such number is outside the range or MAX_SAFE_INTEGER
+if you go past 31 it will still return a number, but the math will be a bit wonky. The numbers will indeed be valid, but they are not guaranteed to actually be the next possible instance of a number with these properties. 
 
 Below are 2 solutions to this problem. The first works but is extremely inefficient. The second will accomplish the same output in milliseconds. They each use a different approach. 
 */
@@ -58,5 +60,5 @@ function powerSumDigTerm2(n) {
   console.log(outputArr[n - 1]);
 }
 
-powerSumDigTerm2(5);
+powerSumDigTerm2(31);
 powerSumDigTerm1(5);
